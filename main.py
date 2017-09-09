d4043e36052f0448a942d1e2d6c2bcfd012f8518
yokotanaohiko <iam4570@example.com>
2017-09-09 12:28:14 +0900

    魚の画像をセグメンテーションするモデルを確認できるようにした

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from flask import (
     redirect,
     url_for,
 )
-from visualizer import gen_image_b64
+from visualizer import load_image, segment, to_base_64
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', **{
-        'initlal_noises': [50] * 10,
-        'image': gen_image_b64([50] * 10)
+        'origin_image': to_base_64(load_image('images/fish.png')),
+        'segmented_image': to_base_64(segment(load_image('images/fish.png'))),
     })
 
 @app.route('/upload_model', methods=['POST'])
@@ -31,19 +31,21 @@ def upload():
         return '失敗しました', 400
     return redirect(url_for('index'))
 
-@app.route('/generate', methods=['GET'])
-def generate():
-    noise_list = [int(request.args.get(str(i))) for i in range(10)]
-    return render_template('index.html', **{
-        'initlal_noises': noise_list,
-        'image': gen_image_b64(noise_list),
-    })
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    file_obj = request.files['file']
+    filename = file_obj.filename
+    if filename[-3:] == 'png':
+        file_obj.save('./images/fish.png')
+    else:
+        return '失敗しました', 400
+    return redirect(url_for('index'))
 
 @app.route('/reset', methods=['GET'])
 def reset():
     return render_template('index.html', **{
-        'initlal_noises': [50] * 10,
-        'image': gen_image_b64([50] * 10)
+        'origin_image': to_base_64(load_image('images/fish.png')),
+        'segmented_image': to_base_64(segment(load_image('images/fish.png'))),
     })
 
 @app.route('/js/<path:path>')
@@ -51,7 +53,7 @@ def send_js(path):
     return send_from_directory('js', path)
 
 @app.route('/favicon.ico')
-def send_image(path):
+def send_image():
     return send_from_directory('images', 'favicon.png')
 
 
--- visualizer.py
+++ visualizer.py
@@ -18,23 +18,33 @@ import chainer.functions as F
 import chainer.links as L
 
 import numpy as np
+from PIL import Image
 
 class Generator(chainer.Chain):
     def __init__(self):
         super(Generator, self).__init__(
-            l0s=L.Linear(10, 7 * 7 * 64),
+            c1s=L.Convolution2D(3, 32, 4, stride=2, pad=1),#28x28 to 14x14
+            c2s=L.Convolution2D(32, 64, 4, stride=2, pad=1),#14x14 to 7x7
             dc1s=L.Deconvolution2D(64, 32, 4, stride=2, pad=1),
-            dc2s=L.Deconvolution2D(32, 3, 4, stride=2, pad=1),
-            bn0s=L.BatchNormalization(7 * 7 * 64),
-            bn1s=L.BatchNormalization(32),
+            dc2s=L.Deconvolution2D(64, 2, 4, stride=2, pad=1),
+            bn2s=L.BatchNormalization(32),
+            bn3s=L.BatchNormalization(64),
+            bn1s=L.BatchNormalization(64),
         )
 
     def __call__(self, z, test=False):
-        h = F.reshape(F.relu(self.bn0s(self.l0s(z), test=test)), (z.data.shape[0], 64, 7, 7))
-        h = F.relu(self.bn1s(self.dc1s(h), test=test))
-        x = (self.dc2s(h))
+        h1 = self.c1s(z)
+        h1 = F.leaky_relu(h1)
+        h1 = self.bn2s(h1, test=test)
+        h2 = self.c2s(h1)
+        h2 = F.leaky_relu(h2)
+        h2 = self.bn3s(h2, test=test)
+        h2 = self.dc1s(h2)
+        h = F.concat((h1, h2), axis=1)#32chan to 64chan
+        h = self.bn1s(h, test=test)
+        h = F.leaky_relu(h)
+        x = F.sigmoid(self.dc2s(h))
         return x
-
 
 def clip_img(x):
 	return np.float32(-1 if x<-1 else (1 if x>1 else x))
@@ -45,14 +55,23 @@ gen.to_cpu()
 model_file = 'model.h5'
 serializers.load_hdf5(model_file, gen)
 
-def gen_image_b64(noise_list):
-    z = np.array(noise_list)
-    noise = (z - 50) / float(100)
-    z = Variable(noise.reshape((1, 10)).astype(np.float32))
+
+def load_image(filepath):
+    img = Image.open(filepath)
+    return img
+
+def segment(img):
+    img = img.resize((28, 28), Image.BILINEAR)
+    image_array = (np.asarray(img).astype('f').transpose(2, 0, 1) + 1) / 128.0 - 1.0
+    zc, zh, zw = image_array.shape
+    z = image_array.reshape(-1, zc, zh, zw).astype(np.float32)
     x = gen(z, test=True)
     x = x.data
-    img_array = ((np.vectorize(clip_img)(x[0,:,:,:])+1)/2 * 255).transpose(1,2,0)
-    image = Image.fromarray(np.uint8(img_array))
+    img_array = ((np.vectorize(clip_img)(x[0,:,:,:])+1) * 128 - 1).transpose(1,2,0)
+    return Image.fromarray(np.uint8(img_array))
+
+
+def to_base_64(image):
     output = BytesIO()
     image.save(output, format='PNG')
     return base64.b64encode(output.getvalue()).decode()
